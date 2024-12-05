1934e2d9c36651acd7ae0193858a54bc01c25c9c
Aaron Jarmusch <jarmusch@udel.edu>
2024-12-04 22:48:10 -0500

    Enabling the GPS Locator

--- app/src/main/java/com/example/ecocompass/MainActivity.java
+++ app/src/main/java/com/example/ecocompass/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.ecocompass;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,6 +17,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
+
+        Button button = findViewById(R.id.buttonGPS);
+//        button.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(MainActivity.this, gps_locator.class);
+//                startActivity(intent);
+//            }
+//        });
+
         setContentView(R.layout.activity_main);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
