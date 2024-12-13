219b4a76b35cd17cdb28c9c150854e1b93f33a65
Aaron Jarmusch <jarmusch@udel.edu>
2024-12-13 17:31:50 -0500

    Fixed Button

--- app/src/main/java/com/example/ecocompass/MainActivity.java
+++ app/src/main/java/com/example/ecocompass/MainActivity.java
@@ -18,20 +18,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
 
-        Button button = findViewById(R.id.buttonGPS);
-//        button.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                Intent intent = new Intent(MainActivity.this, gps_locator.class);
-//                startActivity(intent);
-//            }
-//        });
-
         setContentView(R.layout.activity_main);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        Button button = findViewById(R.id.buttonGPS);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, gps_locator.class);
+                startActivity(intent);
+            }
         });
     }
 }
